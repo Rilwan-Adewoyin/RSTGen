5661bf9a982f3b553721b8dfd0c07a40725d593c
akanni.ade <akanni.ade>
2021-01-04 03:55:56 +0000

    new DA

--- rl_conv/pretrain/DialogueAct/train.py
+++ rl_conv/pretrain/DialogueAct/train.py
@@ -660,7 +660,6 @@ def main(tparams, mparams):
                         default_root_dir=utils.get_path(f"./models/{tparams.version_name}"),
                         precision=16, callbacks=callbacks,
                         limit_train_batches = 0.5 ,
-                        move_metrics_to_cpu= True,
 
                         #track_grad_norm = True,
                         #overfit_batches=5
@@ -678,7 +677,6 @@ def main(tparams, mparams):
                     check_val_every_n_epoch=1, logger=tb_logger,
                     default_root_dir=utils.get_path(f"./models/{tparams.version_name}"),
                     precision=16, callbacks=callbacks ,
-                                            move_metrics_to_cpu= True,
 
                     #track_grad_norm = True,
                     #overfit_batches=5
